f038ac1ca2f9467b548f1a9b659319c4c6672919
rtphokie <q.i43,~sYTiebFrM>
2023-12-25 13:22:03 -0500

    cleanup dead code

--- src/geocitysorter/__init__.py
+++ src/geocitysorter/__init__.py
@@ -1,11 +1,5 @@
-import geopandas as gpd
-import geopy.distance
-import pandas as pd
-import pkg_resources
-from tqdm import tqdm
-
 from .census import census_incorporated_cities
-from .populations import get_census_data
 from .geocitysorter import cities_us, capital_cities, uscb_shapefiles, order_geo_dataframe, calcdist
+from .populations import get_census_data
 
 resource_package = __name__
